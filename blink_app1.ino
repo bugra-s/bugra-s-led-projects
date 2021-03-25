052e5750161e4ce73300f2f144450b915deee83a
bugra <81382952+bugra-s@users.noreply.github.com>
2021-03-25 21:09:11 +0300

    Add files via upload

---
+++ blink_app1.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(7, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(7, HIGH);
+delay(100);
+digitalWrite(7, LOW);
+delay(100);
+
+
+
+
+}
