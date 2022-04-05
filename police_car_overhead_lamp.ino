93402736f1c43a432314cc3a7fbad7c97593eecb
bugra <81382952+bugra-s@users.noreply.github.com>
2022-04-05 22:01:30 +0300

    Add files via upload

---
+++ police_car_overhead_lamp.ino
@@ -0,0 +1,94 @@
+void setup() {
+ pinMode(2, OUTPUT);
+ pinMode(3, OUTPUT);
+ pinMode(4, OUTPUT);
+ pinMode(5, OUTPUT);
+ pinMode(6, OUTPUT);
+ pinMode(7, OUTPUT);
+ pinMode(8, OUTPUT);
+ pinMode(9, OUTPUT);
+ pinMode(10, OUTPUT);
+
+}
+
+void loop() {
+
+digitalWrite(2, HIGH);
+delay(30);
+digitalWrite(2, LOW);
+delay(30);
+digitalWrite(3, HIGH);
+delay(30);
+digitalWrite(3, LOW);
+delay(30);
+digitalWrite(4, HIGH);
+delay(30);
+digitalWrite(4, LOW);
+delay(30);
+digitalWrite(5, HIGH);
+delay(30);
+digitalWrite(5, LOW);
+delay(30);
+digitalWrite(6, HIGH);
+delay(30);
+digitalWrite(6, LOW);
+delay(30);
+digitalWrite(7, HIGH);
+delay(30);
+digitalWrite(7, LOW);
+delay(30);
+digitalWrite(8, HIGH);
+delay(30);
+digitalWrite(8, LOW);
+delay(30);
+digitalWrite(9, HIGH);
+delay(30);
+digitalWrite(9, LOW);
+delay(30);
+digitalWrite(10, HIGH);
+delay(30);
+digitalWrite(10, LOW);
+delay(30);
+
+
+
+digitalWrite(10, HIGH);
+delay(30);
+digitalWrite(10, LOW);
+delay(30);
+digitalWrite(9, HIGH);
+delay(30);
+digitalWrite(9, LOW);
+delay(30);
+digitalWrite(8, HIGH);
+delay(30);
+digitalWrite(8, LOW);
+delay(30);
+digitalWrite(7, HIGH);
+delay(30);
+digitalWrite(7, LOW);
+delay(30);
+digitalWrite(6, HIGH);
+delay(30);
+digitalWrite(6, LOW);
+delay(30);
+digitalWrite(5, HIGH);
+delay(30);
+digitalWrite(5, LOW);
+delay(30);
+digitalWrite(4, HIGH);
+delay(30);
+digitalWrite(4, LOW);
+delay(30);
+digitalWrite(3, HIGH);
+delay(30);
+digitalWrite(3, LOW);
+delay(30);
+digitalWrite(2, HIGH);
+delay(30);
+digitalWrite(2, LOW);
+delay(30);
+
+//END OF THE CODE :) IF THE CODE DOESN'T WORK, PLEASE LEAVE A COMMENT
+ 
+}
